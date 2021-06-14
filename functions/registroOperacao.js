021af564ee882fff43dfdb0dc37b950aa45b0323
Paulo Aragao <paragao@amazon.com>
2021-06-14 17:23:40 -0300

    strict js

--- functions/registroOperacao.js
+++ functions/registroOperacao.js
@@ -3,14 +3,14 @@ exports.handler = async (event) => {
     console.log(event)
     
     for (let key in event) {
-        if event[key] == '/registro-Operacao' { 
+        if (event[key] == '/registro-Operacao') { 
             const response = {
                 statusCode: 200,
                 body: JSON.stringify({ 'sucessoRegistro': 'true' }),
             };
         }
 
-        if event[key] == '/gera-resposta' { 
+        if (event[key] == '/gera-resposta') { 
             const response = {
                 statusCode: 200,
                 body: JSON.stringify({ 'sucessoRegistro': 'true' }),
