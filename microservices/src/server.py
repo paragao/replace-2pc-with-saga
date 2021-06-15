2dcd7329f49df222889335268bc412c4a8be11b7
Paulo Aragao <paragao@amazon.com>
2021-06-15 18:00:20 -0300

    adding post method to microservice

--- microservices/src/server.py
+++ microservices/src/server.py
@@ -1,15 +1,41 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import boto3
 
 server = Flask(__name__)
+sfn = boto3.client('stepfunctions', region_name='us-east-1')
 
 #microserviços
 @server.route("/")
 def root_path():
     return("ok")
 
-@server.route("/teste")
+@server.route("/teste", methods=["POST"])
 def teste():
-    return("chamada PREPARA ENVIA AGENDAS do microserviço")
+
+    event = request.json.get()
+    print(event)
+
+    if (True):
+        sucesso = True
+        output = { 
+            "message": "Prepara Agendas concluído com sucesso"
+        }
+        taskToken = ""
+        response = sfn.send_task_success(taskToken, output)
+
+    else:
+        sucesso = False
+        error = "Falso"
+        cause = "Prepara Agendas não foi concluído com sucesso"
+        taskToken = ""
+
+        response = sfn.send_task_failure(taskToken, error, cause)
+
+    mensagem_retorno = {
+        "taskToken": "",
+        "sucesso": sucesso
+    }
+    return json.dumps(mensagem_retorno)
 
 @server.route("/teste/efetiva-operacao")
 def efetiva_operacao():
